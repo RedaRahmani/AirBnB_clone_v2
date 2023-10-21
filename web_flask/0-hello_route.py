b77910a32ea10898c2379489f753bfe7b2d6360c
RedaRahmani <mohamedredarahmani20@ump.ac.ma>
2023-10-21 16:06:51 -0700

    Fix Task0

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python3
+""" Module documents """
 from flask import Flask
 app = Flask(__name__)
 
 @app.route('/about', strict_slashes=False)
 def Hello():
+    """ hello documentation """
     return "Hello HBNB!"
 
 if __name__ == '__main__':
-    app.run(debug=True)
     app.run(port=5000)
     app.run(host="0.0.0.0")
 
