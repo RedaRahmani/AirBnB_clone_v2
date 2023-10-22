c044df3a2bee298fd80913c10e936b3682531a54
RedaRahmani <mohamedredarahmani20@ump.ac.ma>
2023-10-22 08:48:06 -0700

    add Task05

---
+++ web_flask/5-number_template.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+""" Module documents """
+from flask import Flask
+from flask import render_template
+app = Flask(__name__)
+
+@app.route('/', strict_slashes=False)
+def Hello():
+    """ hello documentation """
+    return "Hello HBNB!"
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """ hbnb documentation """
+    return "HBNB"
+@app.route('/c/<text>', strict_slashes=False)
+def c(text):
+    """ c documentation """
+    formatted_text = text.replace('_', ' ')
+    response_text = f'C {formatted_text}'
+    return response_text
+@app.route('/python', defaults={'text': 'is cool'}, strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def python(text):
+    """ python doc """
+    return 'Python {}'.format(text.replace("_", " "))
+@app.route('/number/<int:n>', strict_slashes=False)
+def number(n):
+    """ c documentation """
+    return f'{n} is a number'
+@app.route('/number_template/<n>', strict_slashes=False)
+def python(n):
+    """ python doc """
+    return render_template('number_template.html', n=n)
+
+if __name__ == '__main__':
+    app.run(port=5000)
+    app.run(host="0.0.0.0")
