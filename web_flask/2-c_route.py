3e23e5e7f8f82f8884c136c310ca1857a84fa32d
RedaRahmani <mohamedredarahmani20@ump.ac.ma>
2023-10-22 05:27:11 -0700

    add task02

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+#!/usr/bin/python3
+""" Module documents """
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/', strict_slashes=False)
+def Hello():
+    """ hello documentation """
+    return "Hello HBNB!"
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """ hbnb documentation """
+    return "HBNB"
+@app.route('/c/<text>', strict_slashes=False)
+def c(text):
+    """ c documentation """
+    formatted_text = text.replace('_', ' ')
+    response_text = f'C {formatted_text}'
+    return response_text
+
+if __name__ == '__main__':
+    app.run(port=5000)
+    app.run(host="0.0.0.0")
