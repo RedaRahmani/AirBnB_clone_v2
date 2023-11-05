7419c1595a13210372cc28099b3a663e590b7476
RedaRahmani <mohamedredarahmani20@ump.ac.ma>
2023-11-05 13:29:08 -0800

    Fix Task05

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -1,37 +1,46 @@
 #!/usr/bin/python3
-""" Module documents """
+""" module doc """
 from flask import Flask
 from flask import render_template
+
 app = Flask(__name__)
 
-@app.route('/', strict_slashes=False)
-def Hello():
-    """ hello documentation """
+
+@app.route("/", strict_slashes=False)
+def hello():
+    """ def doc """
     return "Hello HBNB!"
-@app.route('/hbnb', strict_slashes=False)
+
+
+@app.route("/hbnb", strict_slashes=False)
 def hbnb():
-    """ hbnb documentation """
+    """ def doc """
     return "HBNB"
+
+
 @app.route('/c/<text>', strict_slashes=False)
 def c(text):
-    """ c documentation """
-    formatted_text = text.replace('_', ' ')
-    response_text = f'C {formatted_text}'
-    return response_text
+    """ def doc """
+    return 'c {}'.format(text.replace("_", " "))
+
+
 @app.route('/python', defaults={'text': 'is cool'}, strict_slashes=False)
 @app.route('/python/<text>', strict_slashes=False)
 def python(text):
-    """ python doc """
+    """ def doc """
     return 'Python {}'.format(text.replace("_", " "))
+
+
 @app.route('/number/<int:n>', strict_slashes=False)
 def number(n):
-    """ c documentation """
-    return f'{n} is a number'
-@app.route('/number_template/<n>')
+    """ def doc """
+    return '{} is a number'.format(n)
+
+
+@app.route('/number_template/<int:n>')
 def number_template(n):
-    """ python doc """
     return render_template('5-number.html', number=n)
 
-if __name__ == '__main__':
-    app.run(port=5000)
-    app.run(host="0.0.0.0")
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
