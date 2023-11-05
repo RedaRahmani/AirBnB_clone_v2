814978caf70572878627c3300e9597478a221f55
RedaRahmani <mohamedredarahmani20@ump.ac.ma>
2023-11-05 08:14:45 -0800

    Fix task03

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,62 +1,50 @@
 #!/usr/bin/python3
+""" module doc
 """
-This module uses fabric to
-deploy files to a server
-"""
-
-
-from fabric.api import env, run, put
+from fabric.api import task, local, env, put, run
+from datetime import datetime
 import os
 
+env.hosts = ['18.207.1.87', '52.206.189.175']
 
-env.hosts = ['35.174.211.15', '100.25.199.94']
 
-
-def do_deploy(archive_path):
-    """ This function takes the path of the archive
-        and uploads it to the servers
+@task
+def do_pack():
+    """ method doc
+        sudo fab -f 1-pack_web_static.py do_pack
     """
-    if not os.path.exists(archive_path):
+    formatted_dt = datetime.now().strftime('%Y%m%d%H%M%S')
+    mkdir = "mkdir -p versions"
+    path = "versions/web_static_{}.tgz".format(formatted_dt)
+    print("Packing web_static to {}".format(path))
+    if local("{} && tar -cvzf {} web_static".format(mkdir, path)).succeeded:
+        return path
+    return None
+
+
+@task
+def do_deploy(archive_path):
+    """ method doc
+        fab -f 2-do_deploy_web_static.py do_deploy:
+        archive_path=versions/web_static_20231004201306.tgz
+        -i ~/.ssh/id_rsa -u ubuntu
+    """
+    try:
+        if not os.path.exists(archive_path):
+            return False
+        fn_with_ext = os.path.basename(archive_path)
+        fn_no_ext, ext = os.path.splitext(fn_with_ext)
+        dpath = "/data/web_static/releases/"
+        put(archive_path, "/tmp/")
+        run("rm -rf {}{}/".format(dpath, fn_no_ext))
+        run("mkdir -p {}{}/".format(dpath, fn_no_ext))
+        run("tar -xzf /tmp/{} -C {}{}/".format(fn_with_ext, dpath, fn_no_ext))
+        run("rm /tmp/{}".format(fn_with_ext))
+        run("mv {0}{1}/web_static/* {0}{1}/".format(dpath, fn_no_ext))
+        run("rm -rf {}{}/web_static".format(dpath, fn_no_ext))
+        run("rm -rf /data/web_static/current")
+        run("ln -s {}{}/ /data/web_static/current".format(dpath, fn_no_ext))
+        print("New version deployed!")
+        return True
+    except Exception:
         return False
-
-    file_ext = archive_path[archive_path.find('/') + 1:]
-    file_name = archive_path[archive_path.find('/') + 1: -4]
-
-    result = put(archive_path, '/tmp/' + file_ext)
-    if result.failed:
-        return False
-
-    result = run('mkdir -p /data/web_static/releases/' + file_name + '/')
-    if result.failed:
-        return False
-
-    result = run('tar -xzf /tmp/' + file_ext +
-                 ' -C /data/web_static/releases/' + file_name + '/')
-    if result.failed:
-        return False
-
-    result = run('rm /tmp/' + file_ext)
-    if result.failed:
-        return False
-
-    result = run('mv /data/web_static/releases/' + file_name +
-                 '/web_static/* /data/web_static/releases/' + file_name + '/')
-    if result.failed:
-        return False
-
-    result = run('rm -rf /data/web_static/releases/' + file_name +
-                 '/web_static')
-    if result.failed:
-        return False
-
-    result = run('rm -rf /data/web_static/current')
-    if result.failed:
-        return False
-
-    result = run('ln -s /data/web_static/releases/' +
-                 file_name + '/ /data/web_static/current')
-    if result.failed:
-        return False
-
-    print('New version deployed!')
-    return True
